456fa4ea54b0b7775f1a6a9ea314a85d9d323191
hoangvu1118 <mhoang.vuduc@gmail.com>
2026-03-20 04:06:11 +0700

    create backend folder structure

---
+++ backend/src/main/java/com/vgu/tripify/TripifyApplication.java
@@ -0,0 +1,22 @@
+package com.vgu.tripify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TripifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripifyApplication.class, args);
+        System.out.println("Hello World");
+	}
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World Spring Boot";
+    }
+
+
+}
